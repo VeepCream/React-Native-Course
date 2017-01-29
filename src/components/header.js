6b5bac74e8eac22eeb947721e091e3113529562c
Veerapat Preechapant <VEEP_CREAM@Veerapats-MacBook-Pro.local>
2017-01-29 15:36:57 +0700

    react-native-05

--- src/components/header.js
+++ src/components/header.js
@@ -18,11 +18,21 @@ Header.PropTypes = {
  
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
   }
 };
  
+// เปิดทางให้ component ตัวอื่นๆ สามารถนำ component ตัวนี้ ไปใช้งานได้
 export default Header;
